d08190db2c86111a629bf57a09f4764d67aa0239
CEREBR4L <mattmillergraphics@gmail.com>
2016-10-07 23:28:39 +0100

    Faces n things - for fun

---
+++ face.js
@@ -0,0 +1,20 @@
+
+var FacesNThings = ["i1", "i2", "i3", "i4", "i5", "i6", "i7", "i8", "i9", "i10", "i11", "i12", "i13", "i14", "i15", "i16", "i17", "i18", "i19", "i20", "i21", "i22", "i23"];
+
+document.getElementById('face').classList.add(FacesNThings[Math.floor(Math.random() * FacesNThings.length)]);
+
+document.getElementById('face').addEventListener('click', function () {
+
+	var face = Math.floor(Math.random() * FacesNThings.length);
+
+	//remove the possibility of gettig the same thing twice coz...
+	if(FacesNThings[face] === this.classList[1]){
+		var face = face +  1;
+	}
+
+	this.className = "icon";
+
+	this.classList.add(FacesNThings[face]);
+
+});
+
